f855bb723c2b962d9ba297af2083f0fa81d4a39a
Sunil Kumar Banjare <banjare1990@gmail.com>
2020-03-21 23:49:13 +0530

    i18n implementation

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState }  from 'react';
+import './App.css';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+
+
+function App() {
+  const messages = {
+    en: {
+      greeting: "Hello {name}! How are you?",
+      time: "The time is {t, time, short}.",
+      date: "The date is {d, date}."
+    },
+    es: {
+      greeting: "¡Hola {name}! ¿Cómo estás?",
+      time: "La hora es {t, time, short}.",
+      date: "La fecha es {d, date}."
+    },
+    fr: {
+      greeting: "Bonjour {name}! Comment ça va?",
+      time: "Il est {t, time, short}.",
+      date: "La date est {d, date}."
+    },
+    de: {
+      greeting: "Hallo {name}! Wie geht's?",
+      time: "Es ist {t, time, short} Uhr.",
+      date: "Das Datum ist {d, date}."
+    }
+  };
+  const [name, setName] = useState("");
+
+  const handleChange = e => {
+    setName(e.target.value);
+  };
+
+  const [locale, setLocale] = useState("en");
+
+  const handleSelect = e => {
+    setLocale(e.target.value);
+  };
+
+  return (
+    <>
+      <input placeholder="Enter name" onChange={handleChange} />
+      <select onChange={handleSelect} defaultValue={locale}>
+        {["en", "es", "fr", "de"].map(l => (
+          <option key={l}>{l}</option>
+        ))}
+      </select>
+
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <p>
+          <FormattedMessage id="greeting" values={{ name }} />
+          <br />
+          <FormattedMessage id="time" values={{ t: Date.now() }} />
+          <br />
+          <FormattedMessage id="date" values={{ d: Date.now() }} />
+        </p>
+      </IntlProvider>
+    </>
+  );
+}
+
+export default App;
+ 
